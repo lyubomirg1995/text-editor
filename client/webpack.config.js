cbb8d145ecf8cc74d3fc9747afdbff531f81a2ed
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-11 19:51:17 -0500

    add: dependencies, asset caching, webpack loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,13 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Add and configure workbox plugins for a service worker and manifest file.
+
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+// Add CSS loaders and babel to webpack.
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = () => {
   return {
