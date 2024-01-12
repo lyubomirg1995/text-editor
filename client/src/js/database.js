bd34ab193ef89da8205a6914c322362bae2d9dc7
lyubomirg1995 <lyubomirg17@gmail.com>
2024-01-11 19:12:01 -0500

    add: database methods for content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,34 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+}
+// Method that gets all the content from the database
+export const getDb = async () => {
+   // Database and version
+   const jateDB = await openDB("jate", 1);
+   // New transaction specifying db and privileges
+   const tx = jateDB.transaction("jate", "readonly");
+   // Open desired object store
+   const store = tx.objectStore("jate");
+   // Get all request
+   const request = store.getAll();
+   // Confirmation and return
+   const result = await request;
+   console.log("🚀 - data read from database", result);
+   return result.value;
+}
 
 initdb();
